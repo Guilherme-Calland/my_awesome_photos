231ad2ea1acb2ce3a082f235f608a155d8f37716
guilhermow <gui.crb0@gmail.com>
2020-11-20 09:33:37 -0300

    ADDING DEXTER  permissions

--- app/src/main/java/com/guilhermecallandprojects/myawesomephotos/MainActivity.kt
+++ app/src/main/java/com/guilhermecallandprojects/myawesomephotos/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.guilhermecallandprojects.myawesomephotos
 
+import android.Manifest
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.guilhermecallandprojects.myawesomephotos.fragments.GalleryFragment
 import com.guilhermecallandprojects.myawesomephotos.fragments.MapFragment
 import com.guilhermecallandprojects.myawesomephotos.general.showShortToast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 //TODO: make the app navigate through diferent screens
@@ -27,6 +38,46 @@ class MainActivity : AppCompatActivity() {
         map_btn.setOnClickListener {
             showFragment(MapFragment())
         }
+        fab_btn.setOnClickListener {
+            askForPermissions()
+        }
+    }
+
+    private fun askForPermissions() {
+        Dexter.withContext(this).withPermissions(
+            Manifest.permission.CAMERA
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    showShortToast(this@MainActivity, "Camera usage is granted!")
+                }
+            }
+            override fun onPermissionRationaleShouldBeShown(
+                p0: MutableList<PermissionRequest>?,
+                p1: PermissionToken?
+            ) {
+                showRationalDialogForPermissions()
+            }
+
+        }).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage("It looks like you have turned off permissions required. It can be enables under application settings.")
+            .setPositiveButton("Go to settings")
+            {
+                _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel"){dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
     private fun showFragment(fragment: Fragment) {
